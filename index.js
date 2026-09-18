d639e7d9ee27651dcb98427f9a7033206a2ecf72
Frankrazy <Francisekure26@gmail.com>
2026-09-18 04:03:22 +0300

    Reassigning and incrementing

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 let count = 0;
 console.log(count);
 
-let myAge = 19;
-let humanDogRatio = 7;
-let myDogAge = (myAge * humanDogRatio);
-console.log(myDogAge);
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = (bonusPoints + 50);
+console.log(bonusPoints);
+
+bonusPoints = (bonusPoints - 75);
+console.log(bonusPoints);
+
+bonusPoints = (bonusPoints + 45);
